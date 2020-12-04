4361a8e5e8b4ca24442c028768863deefa36d3ae
Dominik Fleischmann <dominik.fleischmann@canonical.com>
2020-12-04 15:02:37 +0100

    Improve charm unit tests

--- charms/kubernetes-dashboard/tests/unit/test_charm.py
+++ charms/kubernetes-dashboard/tests/unit/test_charm.py
@@ -31,11 +31,14 @@ def test_missing_image(harness):
 
 def test_main_no_relation(harness):
     harness.set_leader(True)
-    harness.add_oci_resource('k8s-dashboard-image', {
-        'registrypath': 'kubernetesui/dashboard:v2.0.4',
-        'username': '',
-        'password': '',
-    })
+    harness.add_oci_resource(
+        "k8s-dashboard-image",
+        {
+            "registrypath": "kubernetesui/dashboard:v2.0.4",
+            "username": "",
+            "password": "",
+        },
+    )
     harness.begin_with_initial_hooks()
     assert isinstance(harness.charm.model.unit.status, ActiveStatus)
     pod_spec = harness.get_pod_spec()
@@ -48,16 +51,28 @@ def test_main_no_relation(harness):
 
 def test_main_with_relation(harness):
     harness.set_leader(True)
-    harness.add_oci_resource('k8s-dashboard-image', {
-        'registrypath': 'kubernetesui/dashboard:v2.0.4',
-        'username': '',
-        'password': '',
-    })
+    harness.add_oci_resource(
+        "k8s-dashboard-image",
+        {
+            "registrypath": "kubernetesui/dashboard:v2.0.4",
+            "username": "",
+            "password": "",
+        },
+    )
     rel_id = harness.add_relation("metrics-scraper", "dashboard-metrics-scraper")
     harness.begin_with_initial_hooks()
     assert isinstance(harness.charm.model.unit.status, WaitingStatus)
     harness.add_relation_unit(rel_id, "dashboard-metrics-scraper/0")
-    harness.update_relation_data(rel_id, "dashboard-metrics-scraper",
-                                 {"service-name": "dashboard-metrics-scraper",
-                                  "service-port": "8000"})
+    harness.update_relation_data(
+        rel_id,
+        "dashboard-metrics-scraper",
+        {"service-name": "dashboard-metrics-scraper", "service-port": "8000"},
+    )
     assert isinstance(harness.charm.model.unit.status, ActiveStatus)
+
+    pod_spec = harness.get_pod_spec()
+    yaml.dump(pod_spec, Dumper=_DefaultDumper)
+    metrics_provider = "--metrics-provider=sidecar"
+    sidecar_host = "--sidecar-host=http://dashboard-metrics-scraper:8000"
+    assert metrics_provider in pod_spec[0]["containers"][0]["args"]
+    assert sidecar_host in pod_spec[0]["containers"][0]["args"]
